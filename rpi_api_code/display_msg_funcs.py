e2ae7ef84481e64f36253fe21f0a0050597225de
Philip Wee <philip.andrewweedewang@gmail.com>
2019-11-02 10:26:57 +0800

    fixed bug

--- rpi_api_code/display_msg_funcs.py
+++ rpi_api_code/display_msg_funcs.py
@@ -158,6 +158,9 @@ def get_msg(url = HOSTURL):
     msg_array = []
     #Remove the irrelevant rows
     for row_no,row in data.iterrows():
+        start = row['msg_start_time']
+        end = row['msg_end_time']
+        repeat = row['repeat']
         if check_is_time(start,end,repeat):
             msg_array.append((row['msg'],row['importance']))
     #Sort the messages by importance
