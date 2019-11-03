35ce60c721dd9db55b45a9712a2c6eca8133ec4e
SirGreat <philip_andrew@mymail.sutd.edu.sg>
2019-11-03 21:18:49 +0800

    Fixed lcd screen not display bug

--- rpi_api_code/display_msg_funcs.py
+++ rpi_api_code/display_msg_funcs.py
@@ -25,47 +25,46 @@ HOSTURL = 'http://169.254.228.228:5000'
 
 is_rpi = True
 #Check if it is an rpi and set up the environment if so
-try:
-    if os.uname().nodename == 'raspberrypi':    
-            
-        #!/usr/bin/python
-        # Example using a character LCD connected to a Raspberry Pi
-        import time
-        import board
-        import digitalio
-        import adafruit_character_lcd.character_lcd as character_lcd
+#try:
+if os.uname().nodename == 'raspberrypi':    
+        
+    #!/usr/bin/python
+    # Example using a character LCD connected to a Raspberry Pi
+    import time
+    import board
+    import digitalio
+    import adafruit_character_lcd.character_lcd as character_lcd
 
-        # Raspberry Pi pin setup
-        lcd_rs = digitalio.DigitalInOut(board.D26)
-        lcd_en = digitalio.DigitalInOut(board.D19)
-        lcd_d7 = digitalio.DigitalInOut(board.D11)
-        lcd_d6 = digitalio.DigitalInOut(board.D5)
-        lcd_d5 = digitalio.DigitalInOut(board.D6)
-        lcd_d4 = digitalio.DigitalInOut(board.D13)
-        lcd_backlight = digitalio.DigitalInOut(board.D2)
+    # Raspberry Pi pin setup
+    lcd_rs = digitalio.DigitalInOut(board.D26)
+    lcd_en = digitalio.DigitalInOut(board.D19)
+    lcd_d7 = digitalio.DigitalInOut(board.D11)
+    lcd_d6 = digitalio.DigitalInOut(board.D5)
+    lcd_d5 = digitalio.DigitalInOut(board.D6)
+    lcd_d4 = digitalio.DigitalInOut(board.D13)
+    lcd_backlight = digitalio.DigitalInOut(board.D2)
 
-        LCD_RS = 26
-        LCD_E  = 19
-        LCD_D4 = 13 
-        LCD_D5 = 6
-        LCD_D6 = 5
-        LCD_D7 = 11
+    LCD_RS = 26
+    LCD_E  = 19
+    LCD_D4 = 13 
+    LCD_D5 = 6
+    LCD_D6 = 5
+    LCD_D7 = 11
 
 
-        # Define LCD column and row size for 16x2 LCD.
-        lcd_columns = 16
-        lcd_rows = 2
+    # Define LCD column and row size for 16x2 LCD.
+    lcd_columns = 16
+    lcd_rows = 2
 
-        lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
-
-except:
+    lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+else:
     is_rpi = False
 
 #This function is for printing a message for a 16x2 LCD
 #Add to this function to handle different display board types
 def print_msg(msg):
     if is_rpi:
-        client.set('msg',msg)
+        lcd.message = msg
     else:
         print(msg)
         
@@ -124,6 +123,7 @@ class time_handler():
             print('Full Message: ', msg.encode())
             print('Printed Message: ',to_print)
             #Set the lcd display
+            print_msg(to_print)
             sys.stdout.flush()
             time.sleep(0.5)
             
